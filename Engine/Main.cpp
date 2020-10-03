37cd177ade9d13de2f8ec60ad479219d00c63df0
Martin Angulo <martin.angulo@digipen.edu>
2020-10-03 14:48:00 +0200

    Added more Materials and cubes.

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -24,6 +24,7 @@
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <map>
 
 #include "src/Gfx/GfxSystem.h"
 #include "src/Gfx/Shader.h"
@@ -66,11 +67,158 @@ struct Light {
   vec3 specular;
 };
 
-std::vector<Material> materials;
+Material emerald;
+Material jade;        
+Material obsidian;	  
+Material pearl;	      
+Material ruby;	      
+Material turquoise;	  
+Material brass;       
+Material bronze;	    
+Material chrome;      
+Material copper;      
+Material gold;	      
+Material silver;	    
+Material blackPlastic;
+Material cyanPlastic; 
+Material greenPlastic;
+Material redPlastic;
+Material whitePlastic;
+Material yellowPlastic;
+Material blackRubber;
+Material cyanRubber;
+Material greenRubber; 
+Material redRubber;
+Material whiteRubber;	
+Material yellowRubber;
+std::map<std::string, Material> materials;
+
 Light light;
 
 int main()
 {
+  emerald.ambient = vec3(0.0215, 0.1745, 0.0215);
+  emerald.diffuse = vec3(0.07568, 0.61424, 0.07568);
+  emerald.specular = vec3(0.633, 0.727811, 0.633);
+  emerald.shininess = 0.1;  
+  jade.ambient = vec3(0.135, 0.222, 0.1575);
+  jade.diffuse = vec3(0.54, 0.89, 0.63);
+  jade.specular = vec3(0.316228, 0.316228, 0.316228);
+  jade.shininess = 0.1;
+  obsidian.ambient = vec3(0.05375, 0.05, 0.06625);
+  obsidian.diffuse = vec3(0.18275, 0.17, 0.22525);
+  obsidian.specular = vec3(0.332741, 0.328634, 0.346435);
+  obsidian.shininess = 0.3;
+  pearl.ambient = vec3(0.25, 0.20725, 0.20725);
+  pearl.diffuse = vec3(1, 0.829, 0.829);
+  pearl.specular = vec3(0.296648, 0.296648, 0.296648);
+  pearl.shininess = 0.088;
+  ruby.ambient = vec3(0.1745, 0.01175, 0.01175);
+  ruby.diffuse = vec3(0.61424, 0.04136, 0.04136);
+  ruby.specular = vec3(0.727811, 0.626959, 0.626959);
+  ruby.shininess = 0.6;
+  turquoise.ambient = vec3(0.1, 0.18725, 0.1745);
+  turquoise.diffuse = vec3(0.396, 0.74151, 0.69102);
+  turquoise.specular = vec3(0.297254, 0.30829, 0.306678);
+  turquoise.shininess = 0.1;
+  brass.ambient = vec3(0.329412, 0.223529, 0.027451);
+  brass.diffuse = vec3(0.780392, 0.568627, 0.113725);
+  brass.specular = vec3(0.992157, 0.941176, 0.807843);
+  brass.shininess = 0.21794872;
+  bronze.ambient = vec3(0.2125, 0.1275, 0.054);
+  bronze.diffuse = vec3(0.714, 0.4284, 0.18144);
+  bronze.specular = vec3(0.393548, 0.271906, 0.166721);
+  bronze.shininess = 0.2;
+  chrome.ambient = vec3(0.25, 0.25, 0.25);
+  chrome.diffuse = vec3(0.4, 0.4, 0.4);
+  chrome.specular = vec3(0.774597, 0.774597, 0.774597);
+  chrome.shininess = 0.6;
+  copper.ambient = vec3(0.19125, 0.0735, 0.0225);
+  copper.diffuse = vec3(0.7038, 0.27048, 0.0828);
+  copper.specular = vec3(0.256777, 0.137622, 0.086014);
+  copper.shininess = 0.1;
+  gold.ambient = vec3(0.24725, 0.1995, 0.0745);
+  gold.diffuse = vec3(0.75164, 0.60648, 0.22648);
+  gold.specular = vec3(0.628281, 0.555802, 0.366065);
+  gold.shininess = 0.4;
+  silver.ambient = vec3(0.19225, 0.19225, 0.19225);
+  silver.diffuse = vec3(0.50754, 0.50754, 0.50754);
+  silver.specular = vec3(0.508273, 0.508273, 0.508273);
+  silver.shininess = 0.4;
+  blackPlastic.ambient = vec3(0.0, 0.0, 0.0);
+  blackPlastic.diffuse = vec3(0.01, 0.01, 0.01);
+  blackPlastic.specular = vec3(0.50, 0.50, 0.50);
+  blackPlastic.shininess = 0.25;
+  cyanPlastic.ambient = vec3(0.0, 0.1, 0.06);
+  cyanPlastic.diffuse = vec3(0.0, 0.50980392, 0.50980392);
+  cyanPlastic.specular = vec3(0.50196078, 0.50196078, 0.50196078);
+  cyanPlastic.shininess = 0.25;
+  greenPlastic.ambient = vec3(0.0, 0.0, 0.0);
+  greenPlastic.diffuse = vec3(0.1, 0.35, 0.1);
+  greenPlastic.specular = vec3(0.45, 0.55, 0.45);
+  greenPlastic.shininess = 0.25;
+  redPlastic.ambient = vec3(0.0, 0.0, 0.0);
+  redPlastic.diffuse = vec3(0.5, 0.0, 0.0);
+  redPlastic.specular = vec3(0.7, 0.6, 0.6);
+  redPlastic.shininess = 0.25;
+  whitePlastic.ambient = vec3(0.0, 0.0, 0.0);
+  whitePlastic.diffuse = vec3(0.55, 0.55, 0.55);
+  whitePlastic.specular = vec3(0.70, 0.70, 0.70);
+  whitePlastic.shininess = 0.25; 
+  yellowPlastic.ambient = vec3(0.0, 0.0, 0.0);
+  yellowPlastic.diffuse = vec3(0.5, 0.5, 0.0);
+  yellowPlastic.specular = vec3(0.60, 0.60, 0.50);
+  yellowPlastic.shininess = 0.25;
+  blackRubber.ambient = vec3(0.02, 0.02, 0.02);
+  blackRubber.diffuse = vec3(0.01, 0.01, 0.01);
+  blackRubber.specular = vec3(0.4, 0.4, 0.4);
+  blackRubber.shininess = 0.078125;
+  cyanRubber.ambient = vec3(0.0, 0.05, 0.05);
+  cyanRubber.diffuse = vec3(0.4, 0.5, 0.5);
+  cyanRubber.specular = vec3(0.04, 0.7, 0.7);
+  cyanRubber.shininess = 0.078125;
+  greenRubber.ambient = vec3(0.0, 0.05, 0.0);
+  greenRubber.diffuse = vec3(0.4, 0.5, 0.4);
+  greenRubber.specular = vec3(0.04, 0.7, 0.04);
+  greenRubber.shininess = 0.078125;
+  redRubber.ambient = vec3(0.05, 0.0, 0.0);
+  redRubber.diffuse = vec3(0.5, 0.4, 0.4);
+  redRubber.specular = vec3(0.7, 0.04, 0.04);
+  redRubber.shininess = 0.078125;
+  whiteRubber.ambient = vec3(0.05, 0.05, 0.05);
+  whiteRubber.diffuse = vec3(0.5, 0.5, 0.5);
+  whiteRubber.specular = vec3(0.7, 0.7, 0.7);
+  whiteRubber.shininess = 0.078125;
+  yellowRubber.ambient = vec3(0.05, 0.05, 0.0);
+  yellowRubber.diffuse = vec3(0.5, 0.5, 0.4);
+  yellowRubber.specular = vec3(0.7, 0.7, 0.04);
+  yellowRubber.shininess = 0.078125;
+
+  materials.insert(std::pair<std::string, Material>("Emerald", emerald));
+  materials.insert(std::pair<std::string, Material>("Jade", jade));
+  materials.insert(std::pair<std::string, Material>("Obsidian", obsidian));
+  materials.insert(std::pair<std::string, Material>("Pearl", pearl));
+  materials.insert(std::pair<std::string, Material>("Ruby", ruby));
+  materials.insert(std::pair<std::string, Material>("Turquoise", turquoise));
+  materials.insert(std::pair<std::string, Material>("Brass", brass));
+  materials.insert(std::pair<std::string, Material>("Bronze", bronze));
+  materials.insert(std::pair<std::string, Material>("Chrome", chrome));
+  materials.insert(std::pair<std::string, Material>("Copper", copper));
+  materials.insert(std::pair<std::string, Material>("Gold", gold));
+  materials.insert(std::pair<std::string, Material>("Silver", silver));
+  materials.insert(std::pair<std::string, Material>("BlackPlastic", blackPlastic));
+  materials.insert(std::pair<std::string, Material>("CyanPlastic", cyanPlastic));
+  materials.insert(std::pair<std::string, Material>("GreenPlastic", greenPlastic));
+  materials.insert(std::pair<std::string, Material>("RedPlastic", redPlastic));
+  materials.insert(std::pair<std::string, Material>("WhitePlastic", whitePlastic));
+  materials.insert(std::pair<std::string, Material>("YellowPlastic", yellowPlastic));
+  materials.insert(std::pair<std::string, Material>("BlackRubber", blackRubber));
+  materials.insert(std::pair<std::string, Material>("CyanRubber", cyanRubber));
+  materials.insert(std::pair<std::string, Material>("GreenRubber", greenRubber));
+  materials.insert(std::pair<std::string, Material>("RedRubber", redRubber));
+  materials.insert(std::pair<std::string, Material>("WhiteRubber", whiteRubber));
+  materials.insert(std::pair<std::string, Material>("YellowRubber", yellowRubber));
+
   //Init
   Engine::GfxSystem gfx;
   gfx.Initialize();
@@ -244,6 +392,7 @@ int main()
   vec3 lightColor(1.0f, 1.0f, 1.0f);
   //vec3 objectColor(1.0f, 0.5f, 0.3f);
   vec3 lightPos(0.0f, 0.0f, 1.0f);
+  vec3 objectPos(0.0f, 0.0f, 0.0f);
 
   //Render loop
   while (!glfwWindowShouldClose(window.GetGLFWHandle())) {
@@ -261,12 +410,14 @@ int main()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
-    lightPos.x = cos(glfwGetTime());
-    lightPos.y = sin(glfwGetTime());
+    //lightPos.x = cos(glfwGetTime());
+    //lightPos.y = sin(glfwGetTime());
 
-    lightColor.x = sin(glfwGetTime() * 2.0f);
-    lightColor.y = sin(glfwGetTime() * 0.7f);
-    lightColor.z = sin(glfwGetTime() * 1.3f);
+    lightPos = camera.GetCamPosition() - camera.GetCamForward();;
+    
+    //lightColor.x = sin(glfwGetTime() * 2.0f);
+    //lightColor.y = sin(glfwGetTime() * 0.7f);
+    //lightColor.z = sin(glfwGetTime() * 1.3f);
 
     //Textures
     //glActiveTexture(GL_TEXTURE0); // activate the texture unit first before binding texture
@@ -312,12 +463,6 @@ int main()
     lightingShader.SetVec3("viewPos", camera.GetCamPosition());
     lightingShader.SetVec3("lightPos", lightPos);
 
-    //Set cube material
-    lightingShader.SetVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-    lightingShader.SetVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-    lightingShader.SetVec3("material.specular", 0.5f, 0.5f, 0.5f);
-    lightingShader.SetFloat("material.shininess", 32.0f);
-
     //Set light
     glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
     glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
@@ -332,13 +477,28 @@ int main()
     lightingShader.SetMat4("projection", projection);
     lightingShader.SetMat4("view", view);
 
-    // world transformation
-    mat4 model = mat4(1.0f);
-    lightingShader.SetMat4("model", model);
 
-    // render the cube
-    glBindVertexArray(cubeVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    mat4 model = mat4(1.0f);
+    u32 cubeCounter = 0;
+    for(auto& mat : materials) {
+      //Set cube material
+      Material cubeMaterial = mat.second;
+      lightingShader.SetVec3("material.ambient", cubeMaterial.ambient);
+      lightingShader.SetVec3("material.diffuse", cubeMaterial.diffuse);
+      lightingShader.SetVec3("material.specular", cubeMaterial.specular);
+      lightingShader.SetFloat("material.shininess", cubeMaterial.shininess * 128.0);
+
+      // world transformation
+      model = mat4(1.0f);
+      model = glm::translate(model, objectPos + vec3(2 * cubeCounter, 0.0f, 0.0f));
+      lightingShader.SetMat4("model", model);
+
+      // render the cube
+      glBindVertexArray(cubeVAO);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+
+      ++cubeCounter;
+    }
 
     // also draw the lamp object
     lightBoxShader.Use();
